9c8f459cc5a6cf36172f3ee2e44f3b93a6956752
haumongto <111073034+haumongto@users.noreply.github.com>
2026-01-26 22:18:06 +0700

    wip: update forgot password page

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log(`Shop page: http://localhost:${PORT}/shop`);
+    console.log(`Login page: http://localhost:${PORT}/login`);
 });
